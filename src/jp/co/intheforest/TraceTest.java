e8767ea42dddbaea88f272056f46e4d06d1bf044
Tomita Kazutaka <tomita@intheforest.co.jp>
2016-04-26 14:12:15 +0900

    add trace log code

--- src/jp/co/intheforest/TraceTest.java
+++ src/jp/co/intheforest/TraceTest.java
@@ -1,9 +1,14 @@
 package jp.co.intheforest;
 
+import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ExecutionInfo;
+import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.QueryTrace;
+
 
 public class TraceTest {
 
@@ -14,10 +19,18 @@ public class TraceTest {
                     .addContactPoint("127.0.0.1")
                     .build();
             Session session = cluster.connect();
-
-            ResultSet rs = session.execute("select release_version from system.local");    // (3)
+//            String sql = "select * from pathee_frontend.\"INVERTED_BARRELS_JP_000\"";
+//            sql = sql + " where \"WORDID\" = ? and \"GKLAT\" = ? AND \"GKLNG\" = ? ";
+            String sql = "select release_version from system.local";
+            PreparedStatement prepared = session.prepare(sql).enableTracing();
+            //BoundStatement bound = prepared.bind("イベント","0635500","1278000");
+            BoundStatement bound = prepared.bind();
+            ResultSet rs = session.execute(bound);
+            ExecutionInfo executionInfo = rs.getExecutionInfo();
+            QueryTrace queryTrace = executionInfo.getQueryTrace();
             Row row = rs.one();
-            System.out.println(row.getString("release_version"));
+            System.out.println(row.toString());
+            System.out.println(queryTrace.getEvents());
         } finally {
             if (cluster != null) cluster.close();
         }
